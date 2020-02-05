4b1c44370b0b2881120bbb2fc95f6bba23b82ace
justindudley <justin.dudley@ascendum.com>
2020-02-04 22:41:00 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+//node app.js    --to spin up
+const express = require("express");
+const myOwnApp = express();
+const port = 3000;
+
+myOwnApp.get("/", (req, res) => res.send("Check out myOwnApp on node.js!"));
+myOwnApp.listen(port, () => console.log(`Yeah, listening on port ${port}!`));
+
+//after running npm install body-parser --save,   must still configure it here:
+var bodyParser = require("body-parser");
+myOwnApp.use(bodyParser.json()); // support json encoded bodies
+myOwnApp.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// --  ||   --  ||  --  \\
+
+//Broswer URL, or Postman Get:  http://localhost:3000/someroute
+myOwnApp.get("/someroute", helloFunction);
+function helloFunction(request, response) {
+  response.send(
+    "Yes, you've reached this text, within the function called helloFunction"
+  );
+}
+
+//Browser URL, or Postman Get:  http://localhost:3000/secondroute?namo=Nitsuj
+myOwnApp.get("/secondroute", useEquals);
+function useEquals(req, res) {
+  var namo = req.query.namo;
+  res.send("The name you gave in your query was " + namo);
+}
+
+//Browser URL, or Postman Get:  http://localhost:3000/welcome/Dudley/Loveland
+myOwnApp.get("/welcome/:name/:city", useInternalColon);
+function useInternalColon(req, res) {
+  var someName = req.params.name;
+  var someCity = req.params.city;
+  res.send("Welcome, " + someName + " from " + someCity + "!");
+}
+
+//Browser URL, or Postman Get:  http://localhost:3000/parity/RUR'
+myOwnApp.get("/parity/:alg", calcParity);
+function calcParity(req, res) {
+  var algo = req.params.alg;
+  var noPrime = algo.replace("'", "");
+  var evenOrOdd = noPrime.length % 2;
+  res.send(
+    evenOrOdd === 0 ? "Your algorithm is even" : "Your algorithm is odd"
+  );
+}
+
+// POST   POST   POST:
+
+// http://localhost:3000/body/
+// JSON sent in body:  {"id": "123", "token": "ah76y8", "geo": "America"}
+myOwnApp.post("/body", bodyFunction);
+
+function bodyFunction(req, res) {
+  var user_id = req.body.id;
+  var token = req.body.token;
+  var geo = req.body.geo;
+
+  res.send(user_id + ", " + token + ".  geo has " + geo.length + " letters");
+}
